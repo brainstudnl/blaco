b16ba8c51f54811aac7b49b108a38c353de965ee
Bram de Vries <44579145+brdv@users.noreply.github.com>
2024-06-25 17:37:35 +0200

    add base ci file (#10)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* eslint-disable-next-line @next/next/no-css-tags */}
         <link
           id="theme-link"
           rel="stylesheet"
